422f9a6cfc464a14344e4a9a5c8be664afbc06cb
Omar del Real Ouldam <zelios@hotmail.es>
2020-08-22 17:35:12 +0200

    Finalizada una versión básica del login page

--- src/views/components/pages/LoginPage.jsx
+++ src/views/components/pages/LoginPage.jsx
@@ -4,11 +4,11 @@ import { Redirect } from 'react-router-dom'
 import styled from 'styled-components'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
+import { useForm } from 'react-hook-form'
 import Container from '../common/Container'
 import Col from '../common/Col'
 import { mediaQueries } from '../../../styles/mediaQueries'
 import Title from '../common/Title'
-// import { useForm } from 'react-hook-form'
 
 const StyledLoginPage = styled(Container).attrs({
   className: 'mt-5',
@@ -33,33 +33,72 @@ const StyledLoginPage = styled(Container).attrs({
 
 const LoginPage = () => {
   const auth = useSelector((state) => state.auth)
-  // const { register } = useForm()
+  const { register, handleSubmit, errors } = useForm()
   const { t } = useTranslation()
 
   /* Si estamos logeados no hay necesidad de mostrar la pagina de login
   por lo cual redireccionamos */
   if (auth.isLoggedIn) return <Redirect to='/' />
 
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
+  // TODO: Separar los input en sus propios componentes (Input)
   return (
     <StyledLoginPage>
       <Helmet title={t('documentHeadTitles.loginPage')} />
       <Col sm={10} md={8} lg={5} className='m-auto'>
         <Title className='title mb-4'>Iniciar sesión</Title>
-        <form>
+        <form
+          className='needs-validation'
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+        >
           <div className='form-group'>
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
-            <label htmlFor='emailInput'>Email address</label>
-            <input type='email' className='form-control' id='emailInput' />
+            <label htmlFor='emailInput'>Correo Electrónico</label>
+            <input
+              name='email'
+              type='email'
+              className={`form-control ${errors.email && 'is-invalid'}`}
+              id='emailInput'
+              ref={register({
+                required: 'Se requiere la dirección de correo electrónico',
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'Correo electrónico inválido',
+                },
+              })}
+            />
+            {errors.email && (
+              <div className='invalid-feedback d-block'>
+                {errors.email.message}
+              </div>
+            )}
           </div>
 
           <div className='form-group'>
             {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
             <label htmlFor='passwordInput'>Password</label>
             <input
+              name='password'
               type='password'
-              className='form-control'
+              className={`form-control ${errors.password && 'is-invalid'}`}
               id='passwordInput'
+              ref={register({
+                required: 'Se requiere la contraseña',
+                minLength: {
+                  value: 4,
+                  message: 'La contraseña debe ser mínimo de 4 caracteres',
+                },
+              })}
             />
+            {errors.password && (
+              <div className='invalid-feedback d-block'>
+                {errors.password.message}
+              </div>
+            )}
           </div>
 
           <button type='submit' className='btn btn-primary mt-4'>
