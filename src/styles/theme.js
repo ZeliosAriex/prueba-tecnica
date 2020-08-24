3f479e84281f98a2eebd4f91be9643e50b6b02e6
Omar del Real Ouldam <zelios@hotmail.es>
2020-08-24 22:31:21 +0200

    * Mejorada página 404

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,13 +1,16 @@
-import { lighten } from 'polished'
+import { lighten, shade } from 'polished'
 
 const theme = {
   primary: '#343a40',
 
   infoColor: '#3090ef',
   infoColorDark: '#1f64a9',
+
+  notFoundBtnColor: '#ff6643'
 }
 
 // Special
 theme.primaryLight = lighten(0.1, theme.primary)
+theme.notFoundBtnColorHover = shade(0.1, theme.notFoundBtnColor)
 
 export default theme
