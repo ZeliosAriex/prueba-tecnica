ebc8b9bf950b06bc70a6a4bd672e80e3d38ce257
Omar del Real Ouldam <zelios@hotmail.es>
2020-08-24 22:31:21 +0200

    * Mejorada página 404

--- src/views/components/pages/NotFoundPage.jsx
+++ src/views/components/pages/NotFoundPage.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import styled from 'styled-components'
+import { useHistory } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
 import { useTranslation } from 'react-i18next'
 import notFoundSVG from '../../../resources/images/404.svg'
 import { mediaQueries } from '../../../styles/mediaQueries'
+import Title from '../common/Title'
+import Button from '../common/Button'
+import MainContainer from '../common/MainContainer'
 
-const NotFoundPage = () => {
-  const { t } = useTranslation()
-
-  return (
-    <Styled404>
-      <Helmet title={t('documentHeadTitles.notFound')} />
-      <img src={notFoundSVG} alt={t('documentHeadTitles.notFound')} />
-      <h1 className='title'>{t('pages.notFound.title')}</h1>
-    </Styled404>
-  )
-}
-
-const Styled404 = styled.div`
+const Styled404 = styled(MainContainer)`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -33,12 +25,34 @@ const Styled404 = styled.div`
     font-size: 1.5rem;
   }
 
+  .btn-not-found {
+    background: ${(p) => p.theme.notFoundBtnColor};
+    color: #fff;
+    margin-top: 2rem;
+    font-size: 1rem;
+    width: 100%;
+
+    &:hover {
+      background: ${(p) => p.theme.notFoundBtnColorHover};
+    }
+  }
+
+  @media ${mediaQueries.sm} {
+    .btn-not-found {
+      width: auto;
+    }
+  }
+
   @media ${mediaQueries.md} {
     img {
       max-width: 37.5rem;
     }
     .title {
       font-size: 1.9rem;
+    }
+
+    .btn-not-found {
+      font-size: 1.2rem;
     }
   }
 
@@ -50,7 +64,33 @@ const Styled404 = styled.div`
     .title {
       font-size: 2.3rem;
     }
+
+    .btn-not-found {
+      margin-top: 2.3rem;
+      font-size: 1.4rem;
+    }
   }
 `
+
+const NotFoundPage = () => {
+  const { t } = useTranslation()
+  const history = useHistory()
+
+  const handleReturnSafeRoute = () => history.push('/')
+
+  return (
+    <Styled404>
+      <Helmet title={t('documentHeadTitles.notFound')} />
+      <img src={notFoundSVG} alt={t('documentHeadTitles.notFound')} />
+      <Title className='title'>{t('pages.notFound.title')}</Title>
+
+      <Button
+        onClick={handleReturnSafeRoute}
+        label={t('pages.notFound.goBackBtn')}
+        className='btn-not-found'
+      />
+    </Styled404>
+  )
+}
 
 export default NotFoundPage
